9a829391ece729895e156de4f61981f04bc4842e
unknown <ozdogan.7547@gmail.com>
2022-12-16 00:13:58 +0300

    Create Route.js

---
+++ src/Route.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { createAppContainer, createSwitchNavigator} from "react-navigation";
+import { createStackNavigator} from "react-navigation-stack";
+import { createBottomTabNavigator} from "react-navigation-tabs";
+
+import Login from './screens/Login/index'
+import AuthRedirect from './screens/AuthRedirect'
+import Rayon from './screens/Reyon/index'
+import Basket from './screens/Sepet/index'
+import AccountStatment from './screens/HesapDokumu/index';
+import Admin from './screens/Admin/index';
+
+const RayonStack = createStackNavigator({
+    Rayon:{screen:Rayon}
+})
+
+const BasketStack = createStackNavigator({
+    Basket:{screen:Basket}
+})
+
+const AccountStatmentStack = createStackNavigator({
+    AccountStatment:{screen:AccountStatment}
+})
+
+const AdminStack = createStackNavigator({
+    Admin:{screen:Admin}
+})
+
+const AuthenticateStack = createStackNavigator({
+    Login: {
+        screen:Login,
+        navigationOptions:{
+            header:null
+        }
+    }
+});
+
+const AppBottomNavigator = createBottomTabNavigator({
+        Rayon:RayonStack,
+        Basket:BasketStack,
+        AccountStatment:AccountStatmentStack,
+        Admin:AdminStack
+})
+
+const SwitchNavigator = createSwitchNavigator({
+    App:AppBottomNavigator,
+    AuthRedirect,
+    Auth:AuthenticateStack
+},{
+    initialRouteName:'AuthRedirect'
+})
+
+export  default createAppContainer(SwitchNavigator);
