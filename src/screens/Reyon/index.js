a631b6d397710d8d19a665221ffaa9cda7da55ce
unknown <ozdogan.7547@gmail.com>
2022-12-16 15:37:03 +0300

    Update index.js

--- src/screens/Reyon/index.js
+++ src/screens/Reyon/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-import {SafeAreaView, ScrollView, Text, TouchableOpacity, View} from "react-native";
+import {SafeAreaView, ScrollView, Text,FlatList, TouchableOpacity, View} from "react-native";
+
+import firestore from "@react-native-firebase/firestore"
 
 export default class Rayon extends React.Component {
 
+    state = {
+        users: '',
+    }
     
     static navigationOptions = ( { navigation }) =>  {
         return {
@@ -18,16 +23,40 @@ export default class Rayon extends React.Component {
               },
         }
     };
+    constructor(props) {
+        super(props);
+        this.getProduct();
+        this.subscriber = firestore().collection('product').get()
+        .then(querySnapshot => {
+          
 
-    constructor() {
-        super();
+            querySnapshot.forEach(documentSnapshot => {
+              users.push({
+                ...documentSnapshot.data(),
+                key: documentSnapshot.id,
+              });
+            });
+      
+            this.setState({users: [this.users]});
+        })
     }
+
+   getProduct = async () => {
+    const userDocument = await firestore().collection("product").get()
+    console.log(userDocument)
+   }
     render() {
         return(
             <SafeAreaView>
-                <ScrollView>
-                        <View><Text>Reyon</Text></View>
-                </ScrollView>
+                    <FlatList
+                    data={this.state.users}
+                    renderItem={({ item }) => (
+                        <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                        <Text>User ID: {item}</Text>
+                      
+                        </View>
+                    )}
+    />
             </SafeAreaView>
         )
     }
