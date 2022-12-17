19cb4568140a0e1661e823429dac05974f01025f
unknown <ozdogan.7547@gmail.com>
2022-12-17 15:45:57 +0300

    Update index.js

--- src/screens/Admin/index.js
+++ src/screens/Admin/index.js
@@ -1,33 +1,97 @@
-import React from "react";
-import {SafeAreaView, ScrollView, Text, TouchableOpacity, View} from "react-native";
+import React, { useState, useContext } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { PracticeContext } from '../Global/PracticeContext';
 
-export default class Admin extends React.Component {
+export default function Admin() {
+    const {
+        kekstra,
+        kekstraPrice,
+        kekstraStock,
+        setKekstra,
+        setkekstraStock,
+        cubukKraker,
+        cubukKrakerStock,
+        cubukKrakerPrice,
+        setCubukKraker,
+        setcubukKrakerStock,
+        sut,
+        sutStock,
+        sutPrice,
+        setSut,
+        setsutStock,
+        amount,
+        setAmount
+    } = useContext(PracticeContext)
 
-    
-    static navigationOptions = () =>  {
-        return {
-            title:'Admin',
-            headerRight:<Text>150 TL</Text>,
-            headerStyle: {
-              backgroundColor: '#d50000',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-        }
-    };
 
-    constructor() {
-        super();
-    }
-    render() {
-        return(
-            <SafeAreaView>
-                <ScrollView>
-                        <View><Text>Admin</Text></View>
-                </ScrollView>
-            </SafeAreaView>
-        )
-    }
+    return (
+        <SafeAreaView>
+            <View style={style.page}>
+                <View style={style.button_footer}>
+                    <Text style={style.text}>KALAN STOKLAR</Text>
+                </View>
+
+                <View style={style.main}>
+                    <View>
+                        <Text style={style.product}>KEKSTRA</Text>
+                        <Text style={style.product}>ÇUBUK KRAKER</Text>
+                        <Text style={style.product}>SÜT</Text>
+                    </View>
+                    <View>
+                        <Text style={style.product}>{kekstraStock}</Text>
+                        <Text style={style.product}>{cubukKrakerStock}</Text>
+                        <Text style={style.product}>{sutStock}</Text>
+                    </View>
+                </View>
+
+                <View style={style.button_borc}>
+                    <Text style={style.text}>BORÇLAR</Text>
+                </View>
+
+                <View style={style.main}>
+                    <View style={style.product}>
+                        <Text style={style.product}>KEKSTRA</Text>
+                        <Text style={style.product}>ÇUBUK KRAKER</Text>
+                        <Text style={style.product}>SÜT</Text>
+                    </View>
+                    <View>
+                        <Text style={style.product}>{kekstraStock}</Text>
+                        <Text style={style.product}>{cubukKrakerStock}</Text>
+                        <Text style={style.product}>{sutStock}</Text>
+                    </View>
+                    <View style={style.borc}>
+                        <Text style={style.state}>ÖDENDİ</Text>
+                        <Text style={style.state}>ÖDENDİ</Text>
+                        <Text style={style.state}>ÖDENDİ</Text>
+                    </View>
+                </View>
+
+                <View style={style.button_footer}>
+                    <Text style={style.text}>TOPLANAN PARA</Text>
+                </View>
+
+                <View style={style.main}>
+                    <View>
+                        <Text>MEVCUT BAKİYE</Text>
+                    </View>
+                    <View>
+                        <Text>{amount} TL</Text>
+                    </View>
+                </View>
+
+            </View>
+        </SafeAreaView>
+    )
 }
+const style = StyleSheet.create({
+    text: { fontWeight: '400', fontSize: 30, marginLeft: 10 },
+    button_footer: { marginVertical: 30, padding: 5, backgroundColor: '#bdbdbd' },
+    main: { display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 30, },
+    page: { marginHorizontal: 30 },
+    borc: {},
+    state: { backgroundColor: '#bdbdbd', paddingHorizontal: 10, paddingVertical: 3, marginBottom: 15 },
+    product: { paddingHorizontal: 10, paddingVertical: 3, marginBottom: 15 , fontWeight:'bold'},
+    button_borc: { marginVertical: 30, padding: 5, marginTop:70, backgroundColor: '#bdbdbd' }
+
+})
+
